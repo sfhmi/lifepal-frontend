d4bbb4b6ba733c9a6c13fd9d07bb074461d47f79
sfhmi <s.fahmii29@gmail.com>
2025-05-14 13:31:33 +0700

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   devIndicators: {
+    appIsrStatus: true,
+    buildActivityPosition: 'bottom-right',
+  },
+  poweredByHeader: false,
+  compress: true,
+  reactStrictMode: false,
+  output: "standalone",
+  httpAgentOptions: {
+    keepAlive: true,
+  },
+  images: {
+    minimumCacheTTL: 36000,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dummyjson.com',
+        port: '',
+        pathname: '/**',
+      },
+    
+    ],
+  },
+  experimental: {
+    optimizeFonts: true,
+  },
+  trailingSlash: false,
+  env: {
+    NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
+  },
+};
 
 module.exports = nextConfig;
